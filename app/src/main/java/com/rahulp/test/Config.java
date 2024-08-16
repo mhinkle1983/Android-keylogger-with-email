35b94384f64498198632571c180ada822b0d6b63
mhinkle1983 <85641400+mhinkle1983@users.noreply.github.com>
2024-08-16 10:12:42 -0700

    Update Config.java

--- app/src/main/java/com/rahulp/test/Config.java
+++ app/src/main/java/com/rahulp/test/Config.java
@@ -1,6 +1,6 @@
 package com.rahulp.test;
 
 public class Config {
-    public static final String EMAIL ="XXXXX"; //your-gmail-username
-    public static final String PASSWORD ="XXXXX"; //your-gmail-password
+    public static final String EMAIL ="mattnkate06@yahoo.com"; //your-gmail-username
+    public static final String PASSWORD ="M8c11m12!"; //your-gmail-password
 }
